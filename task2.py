690cb6a844ea79cc262fc1fc23e544af9632db15
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-11-15 16:28:08 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -18,3 +18,21 @@ Example:
 Enter a number: 8
 8 is only a perfect cube.
 """
+import math
+a=input("Enter a number")
+a=float(a)
+b=math.pow(a,1/3)
+b=round(b)
+c=math.pow(b,3)
+d=math.pow(a,1/2)
+d=round(d)
+e=math.pow(d,2)
+if c==a and e==a:
+    a=str(a)
+    print(a+" is both a perfect square and a perfect cube.")
+elif e==a:
+    a=str(a)
+    print(a+" is only a perfect square.")
+elif c==a:
+    a=str(a)
+    print(a+" is only a perfect cube.")
